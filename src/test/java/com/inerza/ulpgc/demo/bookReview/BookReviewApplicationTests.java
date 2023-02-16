0fd5c4aaa0b357350aaa7dc41e74972b32a5998b
Tinizara Rodriguez <72140889+TinizaraRodriguez@users.noreply.github.com>
2023-02-16 18:10:58 +0000

    initial commit

---
+++ src/test/java/com/inerza/ulpgc/demo/bookReview/BookReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inerza.ulpgc.demo.bookReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
